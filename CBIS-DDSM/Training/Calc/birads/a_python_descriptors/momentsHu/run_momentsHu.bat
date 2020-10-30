2d054e8ef79bd0a6a4f3c8dd8439356f6345d368
JJMacagnan <jlmacagnan@gmail.com>
2020-10-30 17:03:18 -0200

    add CBIS-DDSM tests

---
+++ CBIS-DDSM/Training/Calc/birads/a_python_descriptors/momentsHu/run_momentsHu.bat
@@ -0,0 +1,2 @@
+PrecisionRecall momentsHu_birads_calc_case_description_train_set.txt 9 [+1,2,3,4,5,6,7]
+pause -1
---
+++ CBIS-DDSM/Training/Calc/pathology/a_python_descriptors/haralick/run_haralick.bat
@@ -0,0 +1,2 @@
+PrecisionRecall haralick_pathology_calc_case_description_train_set.txt 15 [+1,2,3,4,5,6,7,8,9,10,11,12,13]
+pause -1
